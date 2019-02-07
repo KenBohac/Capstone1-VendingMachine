97741073967a398360e89fd5ebd97cb70cbd1267
Ken Bohac <bohac91@gmail.com>
2019-02-07 10:08:18 -0500

    product class completed

---
+++ Capstone/Classes/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Classes
+{
+   public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Type { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, decimal price, string type)
+        {
+            int defaultQuantity = 5;
+
+            this.Name = name;
+            this.Price = price;
+            this.Type = type;
+            this.Quantity = defaultQuantity ;
+        }
+
+    }
+}
